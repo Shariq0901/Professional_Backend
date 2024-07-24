89076eda3772d4f0e8393283cdb9d1236eced693
Suraz95 <shaik.suraz50@gmail.com>
2024-07-24 23:24:13 +0530

    prettier

--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
 import express, { urlencoded } from "express";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
-const app = express()
-app.use(express.json({limit:"16kb"}))
-app.use(cors({
+const app = express();
+app.use(express.json({ limit: "16kb" }));
+app.use(
+  cors({
     origin: process.env.Cross_origin,
-    credentials:true
-}))
-app.use(cookieParser())
-app.use(express.urlencoded({ extended: "true", limit: "16kb" }))
-app.use(express.static("public"))
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: "true", limit: "16kb" }));
+app.use(express.static("public"));
 
 //  import Routes
-import userRouter from "./routes/user/user.routes.js"
-import productRouter from "./routes/admin/product.routes.js"
-import visitorRouter from "./routes/visitor/visitor.routes.js"
+import userRouter from "./routes/user/user.routes.js";
+import productRouter from "./routes/admin/product.routes.js";
+import visitorRouter from "./routes/visitor/visitor.routes.js";
 
 // Routes Declaration
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/products", productRouter)
-app.use("/api/v1/visitor",visitorRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/visitor", visitorRouter);
 
-export {app}
+export { app };
