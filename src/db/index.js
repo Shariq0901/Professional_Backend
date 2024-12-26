316a8d4b35208e5d483ca0505918b08bfbf4dcff
SHAIK SURAZ <95890811+Suraz95@users.noreply.github.com>
2024-12-26 12:12:30 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose"
 import {data_base_name} from "../constants.js"
 const connection= async () => {
     try {
+        console.log(process.env.db_url);
         const connectionInstance = await mongoose.connect(
           `${process.env.db_url}/${data_base_name}`
         );
