9067824801cc97b94c2d4aafebf39a46706e7ed1
SHAIK SURAZ <95890811+Suraz95@users.noreply.github.com>
2024-12-26 12:31:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { app } from "./app.js";
 dotenv.config({
   path: "./.env",
 });
+console.log(process.env.PORT)
 connection()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
